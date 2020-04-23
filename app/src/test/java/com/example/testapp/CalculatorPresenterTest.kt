3237d13590365a1d268b6bdcb8172671582cd0ca
Subish <mateusz.vasilewski@gmail.com>
2020-04-23 19:07:18 +0200

    :)

--- app/src/test/java/com/example/testapp/CalculatorPresenterTest.kt
+++ app/src/test/java/com/example/testapp/CalculatorPresenterTest.kt
@@ -1,6 +1,7 @@
 package com.example.testapp
 
 import com.lost.calculator.utils.CalculatorHelper
+import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
@@ -28,25 +29,64 @@ class CalculatorPresenterTest {
     @Test
     @Throws(Exception::class)
     fun addValue_subtraction() {
-        //TODO: fill it
+        calculatorPresenter.addValue("9")
+        calculatorPresenter.addValue("-")
+        calculatorPresenter.addValue("6")
+        calculatorPresenter.addValue("=")
+        Mockito.verify(view).showMessage("3")
     }
 
     @Test
     @Throws(Exception::class)
     fun addValue_division() {
-        //TODO: fill it
+        calculatorPresenter.addValue("10")
+        calculatorPresenter.addValue("/")
+        calculatorPresenter.addValue("2")
+        calculatorPresenter.addValue("=")
+        Mockito.verify(view).showMessage("5")
     }
 
     @Test
     @Throws(Exception::class)
     fun addValue_multiplication() {
-        //TODO: fill it
+        calculatorPresenter.addValue("10")
+        calculatorPresenter.addValue("*")
+        calculatorPresenter.addValue("2")
+        calculatorPresenter.addValue("=")
+        Mockito.verify(view).showMessage("20")
     }
 
     @Test
     @Throws(Exception::class)
     fun addValue_everything() {
-        //TODO: fill it
+        calculatorPresenter.addValue("2")
+        calculatorPresenter.addValue("+")
+        calculatorPresenter.addValue("2")
+        calculatorPresenter.addValue("*")
+        calculatorPresenter.addValue("2")
+        calculatorPresenter.addValue("=")
+        Mockito.verify(view).showMessage("6")
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun custom1() {
+        calculatorPresenter.addValue("0.0001")
+        calculatorPresenter.addValue("+")
+        calculatorPresenter.addValue("0.0001")
+        calculatorPresenter.addValue("=")
+        Mockito.verify(view).showMessage("0.0001")
+        //           :)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun custom2() {
+        calculatorPresenter.addValue("0.001")
+        calculatorPresenter.addValue("+")
+        calculatorPresenter.addValue("0.001")
+        calculatorPresenter.addValue("=")
+        Mockito.verify(view).showMessage("0.002")
     }
 
 }
